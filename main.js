0b3f76bd4682eb46fe5b1ff2b692039a793d91d2
IanBeee <destrianadifa@gmail.com>
2024-11-12 08:15:59 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('my-cache').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/About.html',
+          '/my.html',
+          '/myword.html',
+          '/main.js',
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        return cachedResponse || fetch(event.request);
+      })
+    );
+  });
